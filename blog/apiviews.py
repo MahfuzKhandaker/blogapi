92c4e68ed9ebf15181bc16a0620def5b43febefc
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-11-01 20:11:26 +0600

    commit

--- blog/apiviews.py
+++ blog/apiviews.py
@@ -1,13 +1,12 @@
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.response import Response
 from .models import Category, Post, Comment, Reply
 from .serializers import CategorySerializer, PostSerializer, CommentSerializer, ReplySerializer, UserSerializer
 from .permissions import IsAdminOrReadOnly, IsAuthorOfCommentOrReadOnly, IsAuthorOfReplyOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework import permissions
+
+
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all() 
     serializer_class = CategorySerializer 
